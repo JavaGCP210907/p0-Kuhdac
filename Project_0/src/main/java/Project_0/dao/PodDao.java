585320aba01fe1713cd1fe13219c2c04afc771f7
Kuhdac <87532020+Kuhdac@users.noreply.github.com>
2021-09-22 07:52:10 -0400

    podDao completed

--- Project_0/src/main/java/Project_0/dao/PodDao.java
+++ Project_0/src/main/java/Project_0/dao/PodDao.java
@@ -1,6 +1,7 @@
 package Project_0.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -58,25 +59,117 @@ public class PodDao implements PodDaoInterface{
 
 	@Override
 	public List<Pod> getActivePods() {
-		// TODO Auto-generated method stub
+		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			
+			ResultSet rs = null;
+			
+			String sql = "select from pod where sector_assigned IS NOT NULL";
+			
+			Statement s = conn.createStatement();
+			
+			rs = s.executeQuery(sql);
+			
+			List<Pod> podList = new ArrayList<>();
+			
+			while(rs.next()){
+				
+				Pod p = new Pod(
+						rs.getInt("pod_id"),
+						rs.getDouble("h_count"),
+						rs.getInt("sector_assigned"),
+						rs.getString("industry_assigned"),
+						rs.getBoolean("smile")
+						);
+				
+				podList.add(p);
+			}
+			
+			return podList;
+			
+		} catch(SQLException e) {
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+		}
+				
 		return null;
 	}
 
 	@Override
 	public List<Pod> getInactivePods() {
-		// TODO Auto-generated method stub
-		return null;
+		{
+			
+			try(Connection conn = ConnectionUtil.getConnection()){
+				
+				ResultSet rs = null;
+				
+				String sql = "select from pod where sector_assigned IS NULL";
+				
+				Statement s = conn.createStatement();
+				
+				rs = s.executeQuery(sql);
+				
+				List<Pod> podList = new ArrayList<>();
+				
+				while(rs.next()){
+					
+					Pod p = new Pod(
+							rs.getInt("pod_id"),
+							rs.getDouble("h_count"),
+							rs.getInt("sector_assigned"),
+							rs.getString("industry_assigned"),
+							rs.getBoolean("smile")
+							);
+					
+					podList.add(p);
+				}
+				
+				return podList;
+				
+			} catch(SQLException e) {
+				System.out.println("Connection Failed");
+				e.printStackTrace();
+			}
+					
+			return null;
+		}
 	}
 
 	@Override
 	public void removePod(int pod_id) {
-		// TODO Auto-generated method stub
 		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			
+			String sql = "delete from pod where pod_id = ?";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			ps.setInt(1,pod_id);
+			ps.executeUpdate();
+			
+			System.out.println("Pod:" + pod_id + " has been purged successfully");
+		
+		} catch(SQLException e) {
+			System.out.println("Pod inacessible");
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void addPod(double h_count) {
-		// TODO Auto-generated method stub
+		try (Connection conn = ConnectionUtil.getConnection()){
+			
+			String sql = "insert into pod (h_count) values ?";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			ps.setDouble(1, h_count);
+			ps.executeUpdate();
+			
+		} catch(SQLException e) {
+			System.out.println("Unable to add pod");
+			e.printStackTrace();
+		}
 		
 	}
 
