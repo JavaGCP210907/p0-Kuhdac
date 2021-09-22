d99785f868905bc42910ddff348dac530f92408b
Kuhdac <87532020+Kuhdac@users.noreply.github.com>
2021-09-22 09:15:24 -0400

    completed sectorDao

--- Project_0/src/main/java/Project_0/dao/SectorDao.java
+++ Project_0/src/main/java/Project_0/dao/SectorDao.java
@@ -1,5 +1,50 @@
 package Project_0.dao;
 
-public class SectorDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import Project_0.models.Sector;
+import Project_0.util.ConnectionUtil;
+
+public class SectorDao implements SectorDaoInterface {
+
+	@Override
+	public List<Sector> getSectors() {
+		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			
+			ResultSet rs = null;
+			
+			String sql = "select * from sector";
+			
+			Statement s = conn.createStatement();
+			
+			rs = s.executeQuery(sql);
+			
+			List<Sector> sectorList = new ArrayList<>();
+			
+			while(rs.next()) {
+				
+				Sector d = new Sector();
+					rs.getInt("sector_id");
+					rs.getString("ai_codename");
+					rs.getString("serv_location");
+					
+				sectorList.add(d);
+				
+			}
+			
+			return sectorList;
+			
+		} catch(SQLException e) {
+			System.out.println("Sector connection offline");
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
--- Project_0/src/main/java/Project_0/dao/SectorDaoInterface.java
+++ Project_0/src/main/java/Project_0/dao/SectorDaoInterface.java
@@ -1,5 +1,11 @@
 package Project_0.dao;
 
+import java.util.List;
+
+import Project_0.models.Sector;
+
 public interface SectorDaoInterface {
+	
+	public List<Sector> getSectors();
 
 }
